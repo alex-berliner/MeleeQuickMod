6aebbb7cbf66b9b81d4345f5317b39ead6ac5b71
Alex Berliner <alexberliner@gmail.com>
2023-01-22 13:28:32 -0800

    Smarter file replacement
    - Sort game files by file length during replacement to enable
      "gamefile name in modfile name" string matching
    - Add direct disc file addressing by adding path of a disc file
      inside mods/disc/
    - Add -c argument to dump disc conents to help find file paths

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## About
 
-Melee Quick Mod makes it easy to batch apply mods to a Super Smash Bros. Melee iso. It lets you organize your mods by folders and (semi)intelligently finds the correct files to replace.
+Melee Quick Mod makes it easy to batch apply mods to a Super Smash Bros. Melee iso. It intelligently replaces game files with mod files and lets you organize your mods by folder.
 
 ### Download
 
@@ -8,36 +8,75 @@ Download the latest version from the [releases page](https://github.com/alex-ber
 
 ### Usage
 
-![MQM preview](assets/preview.png?raw=true "MQM preview")
+![MQM preview](assets/preview.gif?raw=true "MQM preview")
 
 The easiest way to use Melee Quick Mod is to download some mods, place them in the `mods` folder, then drag your Melee iso onto `drag_iso_here.bat`. A file named `meleeout.iso` will be created that uses all your mods!
 
-MQM will check all folders inside the `mods` folder, so feel free to organize!
-
-### Advanced
-
-Your mod's filename can have extra info in it besides the name of the file that it's meant to replace in the iso, which you may find helpful for tagging, or just not having to mass rename things when downloading mods. The only rule is that the original filename (case sensitive) has to be in there.
-
-Examples of what MQM will and won't replace:
-
-| Mod Name  | Overwrites Game File | Note |
-| ------------- | ------------- | ------------- |
-| mods\PlFcBu.dat  | files/PlFcBu.dat  |  |
-| mods\PlFcBu Blue Falco.dat  | files/PlFcBu.dat  | Filename can have extra contents |
-| mods\Blue Falco PlFcBu.dat  | files/PlFcBu.dat  | Original filename can be anywhere |
-| mods\falco\PlFcBu.dat  | files/PlFcBu.dat  | Subfolders can be used |
-| mods\Blue Falco.dat   | invalid  | Original filename missing |
-| mods\plfcbu.dat  | invalid  | Original filename case mismatch |
-| mods\PlFcBu  | invalid  | No file extension |
-| mods\PlFcOj.dat  | invalid  | Not a valid color |
-
-The command line arguments let you set the input iso (-i), output iso name (-o), and mods path (-m), and passing the -t flag will append the time to the output iso's filename. You can tweak these by editing `drag_iso_here.bat` or by running from command line.
+MQM checks all folders inside the `mods` folder, so feel free to organize!
 
 ### Linux / Mac
 
-Linux users (and probably Mac but untested) can run the python script directly:
+Linux users (and probably Mac) can run the python script directly:
 
 `./meleequickmod.py -i cleanmelee.iso`
+
+### Mod Naming Rules
+
+For MQM to recognize a mod file to replace a game file, the game filename must exist inside mod filename. You may find this helpful for tagging, or just not having to mass rename things when downloading mods.
+
+Examples of what MQM will and won't replace:
+| Mod Name  | Overwrites Game File | Note |
+| ------------- | ------------- | ------------- |
+| mods\PlFcBu.dat  | files/PlFcBu.dat  | Game file and mod file are the same |
+| mods\PlFcBu Blue Falco.dat  | files/PlFcBu.dat  | Filename can have extra contents |
+| mods\BluePlFcBuFalco.dat  | files/PlFcBu.dat  | Game filename can be anywhere without delimiter|
+| mods\falco\PlFcBu.dat  | files/PlFcBu.dat  | Subfolders can be used |
+| mods\disc\files\audio\us\1padv.ssm  | files/audio/us/1padv.ssm  | [Target specific files](#avoiding-incorrect-file-replacement) |
+| mods\Blue Falco.dat   | invalid  | Original filename missing |
+| mods\plfcbu.dat  | invalid  | Original filename case mismatch |
+| mods\PlFcBu  | invalid  | No file extension |
+| mods\PlFcOj.dat  | invalid  | Not a valid color (See [Avoiding File Misplacement](#avoiding-incorrect-file-replacement)) |
+
+### Command Line Arguments
+
+| Argument | Description |
+| ------------- | ------------- |
+| -h | get help |
+| -i | set input iso |
+| -o | set output iso name |
+| -m | set mods folder |
+| -t | append the time to the output iso's filename |
+| -c | print a list of files on the disc |
+
+You can use these by editing `drag_iso_here.bat` or by running from command line.
+
+### Avoiding Incorrect File Replacement
+
+TL;DR: `PlFcOj.dat` incorrectly replaces `PlFc.dat` instead of not replacing any file. This is intended.
+
+MQM looks to see if the name of any file on the disc exists **inside** of your mod's name. This may lead to incorrect file replacements if, by way of your mod being named incorrectly, it doesn't match to the intended game file, and then does match to a different, unintended game file.
+
+Example: You include a mislabeled mod named `PlFcOj.dat`. Since `Oj` isn't one of Falco's skin colors, MQM performs no file replacement.
+
+Afterwards, MQM looks for a mod file to replace the disc file `PlFc.dat`, Falco's common texture data. Since `PlFc` exists inside `PlFcOj`, MQM incorrectly replaces `PlFcOj.dat` with `PlFc.dat`. Falco's common textures file has been incorrectly been replaced with a skin file and so the game will crash when loading him.
+
+The way to avoid this is to ensure that the **entire text** of the file you wish to replace exists inside the mod filename.
+
+### Structured Disc Replacement
+
+A caveat throwing all your mods in a single folder is that if two files with the same name exist on the disc in different folders MQM won't know which file to replace.
+
+If this happens and you need to target a particular file on the disc, get the exact location of the file on the disc with `.\meleequickmod.exe -i cleanmelee.iso -c`. Then place your file in `mods\disc\my\exact\location\file.dat`. Everything goes in the subfolder `disc\`.
+
+Example: Melee has both `files/audio/1padv.ssm` and `files/audio/us/1padv.ssm`.
+
+Replace `files/audio/us/1padv.ssm` by placing a new `1padv.ssm` at
+
+`mods\disc\files\audio\us\1padv.ssm`
+
+Inexact replacement also works:
+
+`mods\disc\files\audio\us\my_modded_1padv.ssm`
 
 ### Thanks
 
--- meleequickmod.py
+++ meleequickmod.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import sys
+from collections import OrderedDict
 from gcm import *
-import time
 from os import walk
 import argparse
+import time
+import version
 
 def splash():
     print("    __  ___   ____     __  ___")
@@ -11,6 +12,7 @@ def splash():
     print("  / /|_/ /  / / / /  / /|_/ / ")
     print(" / /  / /  / /_/ /  / /  / /  ")
     print("/_/  /_/   \___\_\ /_/  /_/   ")
+    print(f"Version {version.VERSION}")
     print("https://github.com/alex-berliner/MeleeQuickMod")
 
 def process_generator(g):
@@ -42,52 +44,115 @@ def get_name_and_ext(path):
     name = path.split("/")[-1]
     return name.split(".")[0], name.split(".")[-1]
 
-def import_mods(disc_handle, mypath):
-    fails = []
+class ReplaceHandle():
+    def __init__(self, full_path):
+        self.full_path = full_path
+        nandx = get_name_and_ext(full_path)
+        self.dir = get_dir_only(full_path)
+        self.name = nandx[0]
+        self.ext = nandx[1]
+        self.replaced = False
+        self.replaced_with = None
+
+    def __lt__(self, other):
+         return len(self.name) < len(other.name)
+
+    def __str__(self):
+        return f"{self.full_path} {self.name} {self.ext}"
+
+def remove_first_slash(path):
+    return "/".join(path.split("/")[1:])
+
+def get_dir_only(path):
+    if "." not in path:
+        return path
+
+    return "/".join(path.split("/")[:-1])
+
+def replace_disc_conts(disc_handle, mypath, game_files_od, strict_name, disc_replace):
+    mods_applied = set()
     for root, dirs, files in os.walk(mypath):
         for f in files:
-            applied = False
-            mod_name, mod_ext = get_name_and_ext(f)
-            for k in disc_handle.files_by_path:
-                disc_file_name, disc_file_ext = get_name_and_ext(disc_handle.files_by_path[k].file_path)
-                if disc_file_name in mod_name.split(" ") and mod_ext == disc_file_ext:
-                    replace_file(disc_handle, disc_handle.files_by_path[k], os.path.join(root, f))
-                    print(f"Writing {os.path.join(root, f)} over {disc_handle.files_by_path[k].file_path}")
-                    applied = True
-                    break
-            if not applied:
-                fails += [os.path.join(root, f)]
-    if len(fails) > 0:
-        for e in fails:
-            print(f"Could not apply {e}")
+            raw_mod_path = root + "/" + f
+            mod_path = remove_first_slash(raw_mod_path)
+            if not disc_replace and mod_path.startswith("disc/"):
+                continue
+            dict_addr = remove_first_slash(mod_path)
+            mod_dir = get_dir_only(mod_path)
+            mod_name, mod_ext = get_name_and_ext(mod_path)
+            gf = None
+            if dict_addr in game_files_od:
+                gf = game_files_od[dict_addr]
+            else:
+                for k in game_files_od:
+                    gf_name, gf_ext = get_name_and_ext(k)
+                    if gf_name in mod_name and gf_ext == mod_ext:
+                        gf = game_files_od[k]
+                        break
+            if not gf:
+                continue
+            dir_cond = (mod_dir == ("disc/" + gf.dir)) if disc_replace else True
+            name_cond = (gf.name == mod_name) if strict_name else (gf.name in mod_name)
+            if dir_cond and name_cond and gf.ext == mod_ext and not gf.replaced:
+                gf.replaced = True
+                print(f"Writing {mod_path} over {gf.full_path}")
+                replace_file(disc_handle, disc_handle.files_by_path[gf.full_path], raw_mod_path)
+                mods_applied.add(mod_path)
+    return mods_applied
+
+def import_mods(disc_handle, mypath):
+    game_files = [ReplaceHandle(x) for x in disc_handle.files_by_path]
+    game_files.sort(reverse=True)
+
+    game_files_od = OrderedDict((obj.full_path, obj) for obj in game_files)
+    mods_applied = set()
+    mods_applied |= replace_disc_conts(disc_handle, mypath + "/disc", game_files_od, strict_name=True,  disc_replace=True)
+    mods_applied |= replace_disc_conts(disc_handle, mypath + "/disc", game_files_od, strict_name=False, disc_replace=True)
+    mods_applied |= replace_disc_conts(disc_handle, mypath, game_files_od, strict_name=True,  disc_replace=False)
+    mods_applied |= replace_disc_conts(disc_handle, mypath, game_files_od, strict_name=False, disc_replace=False)
+    for root, dirs, files in os.walk(mypath):
+        for f in files:
+            raw_mod_path = root + "/" + f
+            mod_path = remove_first_slash(raw_mod_path)
+            if mod_path not in mods_applied:
+                print(f"Could not apply {raw_mod_path}")
 
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", "--input", default="", help="input iso")
     parser.add_argument("-o", "--output", default="moddedmelee.iso", help="output iso name")
     parser.add_argument("-m", "--mods", default="mods", help="mods storage folder, default is \"mods\"")
-    parser.add_argument("-t", "--time", default=False, help="append time string to output iso name")
+    parser.add_argument("-t", "--time", action='store_true', default=False, help="append time string to output iso name")
+    parser.add_argument("-c", "--contents", action='store_true', default=False, help="list the contents of the disc")
     args = vars(parser.parse_args())
     if args["input"] == "":
         print("You must pass an input iso with -i. Run with -h for help.")
-        sys.exit()
+        exit()
     if args["time"]:
         oname, oext = get_name_and_ext(args["output"])
         args["output"] = f"{oname}{int(time.time())}.{oext}"
+    if args["contents"]:
+        print_disc(disc_handle)
+        exit()
     return args
+
+def print_disc(disc_handle):
+    for f in disc_handle.files_by_path:
+        print(f)
 
 def main():
     splash()
-
     args = parse_args()
     disc_handle = GCM(args["input"])
     disc_handle.read_entire_disc()
     import_mods(disc_handle, args["mods"])
     if len(disc_handle.changed_files) < 1:
         print("No mods were added, aborting.")
-        sys.exit()
+        exit()
     g = disc_handle.export_disc_to_iso_with_changed_files(args["output"])
     process_generator(g)
+    print(f'Wrote output to {args["output"]}')
+
 
 if __name__ == "__main__":
     main()
